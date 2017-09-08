61d20ee2ea7f8e2a556b845562793456e1bc9a66
brentshermana <brentshermana@gmail.com>
2017-09-08 07:14:06 -0700

    designed basis for form

--- WindowsFormsVoiceRecognition/Form1.Designer.cs
+++ WindowsFormsVoiceRecognition/Form1.Designer.cs
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.consolePanel = new System.Windows.Forms.Panel();
+            this.consoleLabel = new System.Windows.Forms.Label();
+            this.consolePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(13, 13);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(40, 13);
+            this.statusLabel.TabIndex = 0;
+            this.statusLabel.Text = "Status:";
+            // 
+            // consolePanel
+            // 
+            this.consolePanel.AutoScroll = true;
+            this.consolePanel.Controls.Add(this.consoleLabel);
+            this.consolePanel.Location = new System.Drawing.Point(13, 30);
+            this.consolePanel.Name = "consolePanel";
+            this.consolePanel.Size = new System.Drawing.Size(440, 219);
+            this.consolePanel.TabIndex = 1;
+            // 
+            // consoleLabel
+            // 
+            this.consoleLabel.AutoEllipsis = true;
+            this.consoleLabel.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.consoleLabel.Location = new System.Drawing.Point(4, 4);
+            this.consoleLabel.Name = "consoleLabel";
+            this.consoleLabel.Size = new System.Drawing.Size(433, 215);
+            this.consoleLabel.TabIndex = 0;
+            this.consoleLabel.Text = "\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 261);
+            this.Controls.Add(this.consolePanel);
+            this.Controls.Add(this.statusLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.consolePanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Panel consolePanel;
+        private System.Windows.Forms.Label consoleLabel;
     }
 }
 
